b3868a9c8430513e89b65502c8b3343d6746df51
Diogo Veigas <202200879@estudantes.ips.pt>
2024-01-27 15:07:26 +0000

    updated icons updated values and names in xml values and respective layouts new navigation bottom bar

--- app/src/main/java/com/example/myfitnessbuddy/MainActivity.java
+++ app/src/main/java/com/example/myfitnessbuddy/MainActivity.java
@@ -6,31 +6,48 @@ import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
-import android.widget.ImageButton;
 import android.widget.FrameLayout;
 
-import com.example.myfitnessbuddy.fragments.FragmentPainel;
+import com.example.myfitnessbuddy.fragments.FragmentDiary;
+import com.example.myfitnessbuddy.fragments.FragmentFoods;
+import com.example.myfitnessbuddy.fragments.FragmentPanel;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
-    FrameLayout fragment_container;
-    ImageButton bt_home;
+    private FrameLayout fragmentContainer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        fragment_container = findViewById(R.id.fragment_container);
-        bt_home = findViewById(R.id.bt_home);
+        fragmentContainer = findViewById(R.id.fragment_container);
 
-        // Pass the instance to updateFragment
-        updateFragment(FragmentPainel.newInstance("abc", "def"));
+        setFragmentNavigation();
     }
 
     private void updateFragment(Fragment fragment) {
         FragmentManager fragmentManager = getSupportFragmentManager();
         FragmentTransaction ft = fragmentManager.beginTransaction();
-        ft.replace(fragment_container.getId(), fragment);
+        ft.replace(fragmentContainer.getId(), fragment);
         ft.commit();
+    }
+
+    private void setFragmentNavigation(){
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setOnItemSelectedListener(item -> {
+            if(item.getItemId() == R.id.bt_home) {
+                updateFragment(FragmentPanel.newInstance());
+                return true;
+            }else if(item.getItemId() == R.id.bt_diary) {
+                updateFragment(FragmentDiary.newInstance());
+                return true;
+            }else if(item.getItemId() == R.id.bt_foods){
+                updateFragment(FragmentFoods.newInstance());
+                return true;
+            }else{
+                return false;
+            }
+        });
     }
 }
--- app/src/main/java/com/example/myfitnessbuddy/fragments/FragmentPanel.java
+++ app/src/main/java/com/example/myfitnessbuddy/fragments/FragmentPanel.java
@@ -12,17 +12,17 @@ import com.example.myfitnessbuddy.R;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link FragmentDiary#newInstance} factory method to
+ * Use the {@link FragmentPanel#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class FragmentDiary extends Fragment {
+public class FragmentPanel extends Fragment {
 
-    public FragmentDiary() {
+    public FragmentPanel() {
         // Required empty public constructor
     }
 
-    public static FragmentDiary newInstance() {
-        FragmentDiary fragment = new FragmentDiary();
+    public static FragmentPanel newInstance() {
+        FragmentPanel fragment = new FragmentPanel();
         Bundle args = new Bundle();
         fragment.setArguments(args);
         return fragment;
@@ -37,6 +37,6 @@ public class FragmentDiary extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_diary, container, false);
+        return inflater.inflate(R.layout.fragment_panel, container, false);
     }
 }
